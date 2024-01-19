ca63f5e9060494df07ff24681c868d88fabb9b17
Shalini Chandran <b022210058@student.utem.edu.my>
2024-01-19 13:20:16 +0800

    shalini

---
+++ attendance.js
@@ -0,0 +1,29 @@
+app.post('/attendance', async (req, res) => {
+    const { matrix, date, subject, section } = req.body;
+    try {
+      attendanceModule.recordAttendance(matrix, date, subject, section);
+      res.status(201).send("Attendance Submitted");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(Error ,{error});
+    }
+  });
+    async function recordAttendance(matrix, date, subject, section){
+      try{
+        const database = client.db ('BENR2423');
+        const collection = database.collection('attendance') ;
+        
+        const user ={
+          matrix: matrix,
+          date :date ,
+          subject:subject,
+          section:section,
+          };
+        
+          await collection.insertOne(user);
+          console.log("Attendance Submitted Successfully");
+        }
+        catch(error){
+          console.log("Attendance already exists")
+          }
+          }
--- student.js
+++ student.js
@@ -1,10 +1,12 @@
-app.get('/viewreport', async (req, res) => { 
-  try { 
-    const db = client.db("BENR2423"); 
-    const viewreport = await db.collection("user").findOne().toArray(); 
-    console.log(viewreport); 
-  } catch (error) { 
-    res.send('Error viewing'); 
-  } 
-}); 
- 
+app.post("/view-student-list, second, async (req.res) => {
+  try {
+    const list = await viewStudentList();
+    console.log(list);
+    return res.status(201).send("View successfully completed");
+
+  }
+  catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+
